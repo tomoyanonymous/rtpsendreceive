1513762a397930a70edbbe6f462cec0961f0ebfb
tomoyanonymous <anon.spyc@gmail.com>
2020-03-25 15:11:54 +0900

    writing sender code

--- source/rtpsendreceive_lib/rtpsendreceive_lib.cpp
+++ source/rtpsendreceive_lib/rtpsendreceive_lib.cpp
@@ -1,4 +1,70 @@
-//  A simple ffmpeg wrapper to send/receive audio stream with rtp protocol written in C++.
+//  A simple ffmpeg wrapper to send/receive audio stream with rtp protocol
+//  written in C++.
 // Copyright 2020 Tomoya Matsuura All rights Reserved.
 // This source code is released under LGPL lisence.
 #include "rtpsendreceive_lib.hpp"
+
+RtpSender::RtpSender(size_t bufsize, int samplerate, int channels)
+    : bufsize(bufsize), samplerate(samplerate), channels(channels) {}
+void RtpSender::init() {
+  avformatctx = avformat_alloc_context();
+\
+  //   av_sdp_create(avformatctx, int n_files, char *buf, int size);
+
+  avioctx = avio_alloc_context(buf_address, bufsize, 0, userdata_address,
+                               RtpSender::readPacket, nullptr, nullptr);
+  avformatctx->pb = avioctx;
+  auto fmt = av_guess_format("rtp", nullptr, "audio/wav");
+  avformatctx->oformat = fmt;
+
+  codec = avcodec_find_encoder(AV_CODEC_ID_PCM_S16BE);
+  auto codecctx = avcodec_alloc_context3(codec);
+  if (codecctx == nullptr) {
+    std::cerr << "avcodec_alloc_context3 failed\n";
+  }
+
+  codecctx->sample_rate = samplerate;
+  codecctx->channels = channels;
+  codecctx->audio_service_type = AV_AUDIO_SERVICE_TYPE_MAIN;
+  // generate global header when the format requires it
+  if (fmt->flags & AVFMT_GLOBALHEADER) {
+    codecctx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+  }
+  AVDictionary *codecoptions = nullptr;
+  // if options are needed....
+  // av_dict_set(&codecoptions,"key","val",0);
+
+  avcodec_open2(codecctx, codec, &codecoptions);
+  snprintf(avformatctx->url, sizeof(avformatctx->url), "rtp://%s:%d",
+           "127.0.0.1", 30000);
+  avstream = avformat_new_stream(avformatctx, codec);
+  auto res_setparams =
+      avcodec_parameters_from_context(avstream->codecpar, codecctx);
+  if (res_setparams < 0) {
+    std::cerr << "avcodec_parameters_from_context failed\n";
+  }
+  auto res_writeheader = avformat_write_header(avformatctx, nullptr);
+  if (res_writeheader < 0) {
+    std::cerr << "avformat_write_header failed\n";
+  }
+  av_dump_format(avformatctx, 0, avformatctx->url, 1);
+  //   int ret = avformat_open_input(&avformatctx, nullptr, nullptr, nullptr);
+  //   if (ret < 0) {
+  //     std::cerr << "Could not open input\n";
+  //   }
+  //   ret = avformat_find_stream_info(avformatctx, nullptr);
+  //   if (ret < 0) {
+  //     std::cerr << "Could not find stream infos\n";
+  //   }
+  av_dump_format(avformatctx, 0, "test_filename", 0);
+  av_init_packet(packet);
+}
+
+int RtpSender::readPacket(void *opaque, uint8_t *buf, int buf_size) {
+  return buf_size;
+}
+void RtpSender::sendData(rtpsender::sample_t *input) {
+  AVFrame *frame;
+  av_read_frame(avformatctx, packet);
+  packet->av_interleaved_write_frame(avformatctx, AVPacket *)
+}
--- source/rtpsendreceive_lib/rtpsendreceive_lib.hpp
+++ source/rtpsendreceive_lib/rtpsendreceive_lib.hpp
@@ -1,9 +1,47 @@
-//  A simple ffmpeg wrapper to send/receive audio stream with rtp protocol written in C++.
+//  A simple ffmpeg wrapper to send/receive audio stream with rtp protocol
+//  written in C++.
 // Copyright 2020 Tomoya Matsuura All rights Reserved.
 // This source code is released under LGPL lisence.
-#include "libavformat/avformat.h"
+#include <iostream>
 #include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
 
+namespace rtpsender {
+using sample_t = int16_t;
+
+struct buffer_data{
+    sample_t* ptr;
+    size_t size;
+};
+
+} // namespace rtpsender
 class RtpSender {
+public:
+  RtpSender(size_t bufsize=128,int samplerate=48000,int channels=1);
+  ~RtpSender();
+  void init();
+  const size_t getBufsizeInRawBytes(){
+      return sizeof(rtpsender::sample_t)*bufsize;
+  }
+  void sendData(rtpsender::sample_t* input);
+private:
+
+  const int samplerate;
+  const int channels;
+  AVCodecContext *avcodecctx;
+  AVFormatContext *avformatctx;
+  AVStream* avstream;
+  AVCodec* codec;
+  static constexpr char* codec_name = "pcm_s16be";
+  AVPacket* packet;
+  AVIOContext *avioctx;
+  rtpsender::buffer_data buffer;
+  unsigned char* buf_address;
+  void* userdata_address;
+  const size_t bufsize;
+
+  AVCodecID codecid;
+  // for reading audio data from streaming, to pass avioctx.
+  static int readPacket(void *opaque, uint8_t *buf, int buf_size);
 
 };
