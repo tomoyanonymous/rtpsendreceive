1398f2ef5804b9de9bb6e8b3d98d4460b69bbe75
Tomoya Matsuura(MacBookPro) <me@matsuuratomoya.com>
2020-10-30 16:53:32 +0900

    windows script

--- build_ffmpeg_win.sh
+++ build_ffmpeg_win.sh
@@ -10,28 +10,15 @@ wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0-win64.exe -O /bin/
 pushd /tmp/ffmpeg
 
 ./configure \
-# --disable-avfoundation \
---disable-iconv \
---disable-filters \
---disable-devices \
---disable-shared \
---enable-static  \
---disable-optimizations  \
---disable-mmx \
---disable-audiotoolbox \
---disable-videotoolbox \
---disable-stripping   \
---disable-appkit \
---disable-zlib \
---disable-coreimage  \
---disable-bzlib \
---disable-securetransport \
---disable-sdl2 \
---disable-lzma \
-# --enable-libopus \
---arch=amd64 \
---cpu=amd64 \
---toolchain=msvc 
+    --disable-gpl \
+    --disable-filters \
+    --disable-devices \
+    --disable-shared \
+    --enable-static  \
+    --enable-x86asm \
+    --arch=amd64 \
+    --cpu=amd64 \
+    --toolchain=msvc 
 
 make -j
 
